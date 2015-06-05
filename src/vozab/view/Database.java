0b14193cb8d829b030faaeafc9ae8a8de5794900
Pawel Jankowski <jjpikoov@gmail.com>
2015-06-05 13:39:07 +0200

    Database createList method implemented.

--- src/vozab/view/Database.java
+++ src/vozab/view/Database.java
@@ -1,8 +1,24 @@
 package vozab.view;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.Console;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+
 public class Database extends SimpleFrame
 {
-	public Database() 
+	private JList<String> recordsList;
+	private DefaultListModel<String> recordsListModel;
+	private JButton addButton;
+	private JButton renameButton;
+	private JButton deleteButton;
+
+
+    public Database() 
 	{
 		super("Database", 400, 400);
 	}
@@ -12,6 +28,39 @@ public class Database extends SimpleFrame
 	{
 		super.setVisible();
 	}
+	
+
+	public void createList(String[] records)
+	{
+		//Maybe it could be done without DefaultListModel
+		recordsListModel = new DefaultListModel<String>();
+		
+		for (String record : records) {
+			recordsListModel.addElement(record);
+		}
+		
+		recordsList = new JList<String>(recordsListModel);
+
+		recordsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+	}
+	
+	
+	
+	
+	
+	
+
+	
+
+		
+
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
